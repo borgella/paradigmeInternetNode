9267d27e78f2317a41a940cb30de7aa5bc8927fd
Borgella JM <jmborgella@hotmail.com>
2016-09-01 13:19:42 -0400

    "Adding routes and modules in app.js"

--- app.js
+++ app.js
@@ -3,7 +3,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-
-
+var environnement = require('./configuration/environnement');
+var login = require('./src/routes/login');
 
 var app = express();
+
+app.use(bodyParser.Json());
+app.use(bodyParser.urlenconded({extended: false}));
+app.use(cookieParser());
+
+//The use of the routers
+
+app.use('/',login);
+
+app.listen(environnement.PORT,function(){
+    console.log('app is listening at port' + environnement.Port);
+});
+
+//console.log('app is listening at port'+ app.listen(environnement.Port,function(){}).adress().adress);
