1f3eb98c9499c26fc6ee39420da2ac91f4c13195
Borgella JM <jmborgella@hotmail.com>
2016-09-01 18:02:13 -0400

    Refactoring of getDatabaseConnection method

--- configuration/database.js
+++ configuration/database.js
@@ -3,6 +3,7 @@
 var environnement = require('./environnement');
 var mongoose = require('mongoose');
 var hateoas = require('../service/hateoas');
+var response = require('../scr/views/responseJson');
 
 module.exports.getDataBaseConnection = function(req,res,next){
     if(mongoose.connection.readyState === 1){
@@ -11,8 +12,8 @@ module.exports.getDataBaseConnection = function(req,res,next){
     }else{
         mongoose.connect(environnement.DATABASE_URL,function(err){
             if(err){
-                res.status(400).end("Something went wrong, we can not connect to the database...!");
-                hateoas.link("home",{});
+                res.status(400)
+                .send(response.responseJson(false,"Something went wrong,cant connect to the database...!",hateoas.link("home",{})));
                 next();
             }else{
                 console.log("Sucessfuly connected to " +environnement.DATABASE_URL +"...!");
