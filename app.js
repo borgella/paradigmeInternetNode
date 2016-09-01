8b479280a4e52e985470eda23e41162491a93439
Borgella JM <jmborgella@hotmail.com>
2016-09-01 14:17:17 -0400

    Fixing the app server and adding default message

--- app.js
+++ app.js
@@ -4,20 +4,30 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var environnement = require('./configuration/environnement');
-var login = require('./src/routes/login');
+var login = require('./scr/routes/login');
 
 var app = express();
 
-app.use(bodyParser.Json());
-app.use(bodyParser.urlenconded({extended: false}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 //The use of the routers
-
 app.use('/',login);
 
-app.listen(environnement.PORT,function(){
-    console.log('app is listening at port' + environnement.Port);
+// Default express error handling 
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-//console.log('app is listening at port'+ app.listen(environnement.Port,function(){}).adress().adress);
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+console.log('App is listening at port '+ app.listen(environnement.PORT,function(){}).address().port);
--- scr/routes/login.js
+++ scr/routes/login.js
@@ -1,1 +1,9 @@
 "use strict"
+var express = require('express');
+var router = express.Router();
+
+router.get('/',function(req,res,next){
+    res.send("Welcome to a limit version of tweeter.");
+});
+
+module.exports = router;
