2e90a118bb796c8b1fedbde6e28e684eed7acc00
Borgella JM <jmborgella@hotmail.com>
2016-09-07 10:45:18 -0400

    ignored test file

--- test/index.js
+++ test/index.js
@@ -1,2 +1,14 @@
 "use strict"
 
+var chai = require('chai');
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+
+
+describe('Blobs', function() {
+  it('should list ALL blobs on /blobs GET');
+  it('should list a SINGLE blob on /blob/<id> GET');
+  it('should add a SINGLE blob on /blobs POST');
+  it('should update a SINGLE blob on /blob/<id> PUT');
+  it('should delete a SINGLE blob on /blob/<id> DELETE');
+});
