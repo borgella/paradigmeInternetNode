2d703f0b7e7366d89a9e49179da00e99191a512b
Borgella JM <jmborgella@hotmail.com>
2016-09-10 19:59:54 -0400

    Empty body add for teste purpose

--- test/index.js
+++ test/index.js
@@ -7,7 +7,8 @@ var assert = require('chai').assert;
 var server = require('../configuration/environnement').BASE_URL;
 
 var request = chai.request(server);
-
+var environnement = require('../configuration/environnement');
+var MockUser = require('./mock/MockUser');
 
 describe('Home',function(){
   it('Get / ',function(done){
@@ -18,4 +19,35 @@ describe('Home',function(){
         done();
       });
   });
+});
+
+describe('User',function(){
+
+    describe('POST /signup',function(){
+        it('Should create a new account for the user',function(done){
+            request.post('/users/signup')
+            .send(MockUser.ValidUser())
+            .end(function(err,res){
+                var expect = 201;
+                var actual = res.status;
+                assert.equal(expect,actual);
+                assert.isTrue(res.body.success);
+                assert.isNotNull(res.body.body);
+                done();
+              })
+       });
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
