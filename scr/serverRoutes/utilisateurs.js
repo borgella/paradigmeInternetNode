7b6a082342ad5fd6c3d459c5e3d254427d385f01
Borgella JM <jmborgella@hotmail.com>
2016-09-11 01:07:13 -0400

    Setting how to receive the user id in the request

--- scr/serverRoutes/utilisateurs.js
+++ scr/serverRoutes/utilisateurs.js
@@ -7,11 +7,14 @@ var hateoas = require('../../service/hateoas');
 var util = require('../../service/util');
 
 
-router.get('/fil',function(req,res,next){
+router.get('/fil',getFil,function(req,res,next){
     res.status(200)
     .send(response.responseJson(true,"Utilisateur requests",hateoas.link("home",{})));
 });
 
-
+function getFil(req,res,next){
+    console.log(req.headers._id);
+    next();
+}
 
 module.exports = router;
