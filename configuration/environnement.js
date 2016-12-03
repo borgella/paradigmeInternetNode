0a23e29484df71e6f619c206007fb81922b05d99
borgella <jmborgella@hotmail.com>
2016-12-03 14:24:44 -0500

    added params to path routes in environnement js

--- configuration/environnement.js
+++ configuration/environnement.js
@@ -7,7 +7,7 @@ module.exports = {
     SECRET: process.env.SECRET || 'si yo di w yo we m ou met di se pa vre',
     CLIENT_ID: process.env.CLIENT_ID || 322608071422956,
     CLIENT_SECRET: process.env.CLIENT_SECRET || '9ba5a624c5392b9fb26a89e66a4abe63',
-    PATH: ['/users/signup' , '/users/login', '/utilisateurs/abonnements/', '/utilisateurs/suggestions/']
+    PATH: ['/users/signup' , '/users/login', '/utilisateurs/abonnements/:_id/:_idsub', '/utilisateurs/suggestions/:_id']
         
 }
 
