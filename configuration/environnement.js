ef43083206caea4d6ef32519ea0f168a7911b070
borgella <jmborgella@hotmail.com>
2016-12-03 00:55:36 -0500

    added mlab DB link to the project

--- configuration/environnement.js
+++ configuration/environnement.js
@@ -3,7 +3,7 @@
 module.exports = {
     PORT: process.env.PORT || 4000,
     BASE_URL: process.env.BASE_URL || 'http://localhost:4000',
-    DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost:27017/paradigmedb',
+    DATABASE_URL: process.env.DATABASE_URL || 'mongodb://borgella:paradigmedb@ds119748.mlab.com:19748/paradigmedb',
     SECRET: process.env.SECRET || 'si yo di w yo we m ou met di se pa vre',
     CLIENT_ID: process.env.CLIENT_ID || 322608071422956,
     CLIENT_SECRET: process.env.CLIENT_SECRET || '9ba5a624c5392b9fb26a89e66a4abe63',
@@ -18,3 +18,5 @@ module.exports.headerAccess = function(req,res,next){
     res.header('Access-Control-Allow-Headers', 'Authorization,Content-Type');
     next();
 }
+
+/**'mongodb://localhost:27017/paradigmedb' */
