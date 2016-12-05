4730efcbf82a76bf442e3d9f7d9088e55011e9a9
borgella <jmborgella@hotmail.com>
2016-12-04 23:05:29 -0500

    change in app.js

--- app.js
+++ app.js
@@ -43,11 +43,12 @@ app.use('/users', users);
 app.use('/users/', signup);
 app.use('/utilisateurs', utilisateurs);
 
-app.use(express.static(__dirname + '/public'));
-app.get('/', function (req, res, next) {
-    res.status(200)
-        .sendFile('/index.html');
-});
+app.use('/', express.static(__dirname + '/public'));
+
+//app.get('/', function (req, res, next) {
+  //  res.status(200)
+    //    .sendFile('/index.html');
+//});
 //app.use('/', index);
 
 
