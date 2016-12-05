67b8d7cec0450048209254c85094aea49f66c220
borgella <jmborgella@hotmail.com>
2016-12-05 00:29:33 -0500

    fixed index.js error in router.get home routes

--- scr/serverRoutes/index.js
+++ scr/serverRoutes/index.js
@@ -14,15 +14,11 @@ router.get('/auth/facebook/callback', passport.authenticate('facebook',
 );
 
 router.get('/', function (req, res, next) {
-    res.status(200)
-        .sendFile(path.join(__dirname, 'public', 'index.html'));
-    // Read the file provided and either return the contents
-    // in data or an err
     fs.readFile('../../public/index.html', function (err, data) {
         if (err) {
             return console.error(err);
         }
-        res.send("The File : " + data.toString());
+        res.status(200).send("The File : " + data.toString());
     });
 });
 
