039fe707cb5ea98765ce186ab0c1a338be891820
borgella <jmborgella@hotmail.com>
2016-12-13 03:01:10 -0500

    console log upload file with formidable

--- scr/serverRoutes/upload.js
+++ scr/serverRoutes/upload.js
@@ -7,31 +7,28 @@ var hateoas = require('../../service/hateoas');
 var util = require('../../service/util');
 var userDaoImpl = require('../model/userDaoImpl');
 var environnement = require('../../configuration/environnement');
+var Formidable = require('formidable');
 
 var AWS = require('aws-sdk');
 
-router.post('/upload', uploadFile, function(req, res, next) {
+router.post('/upload', formidableReading, function(req, res, next) {
     res.status(201)
-        .send(response.responseJson(true, 'Upload file', null, hateoas.link('home',{})));
+        .send(response.responseJson(true, 'upload', null, hateoas.link('home',{})));
 });
 
-var keyName = 'hello_world.txt';
 
-function uploadFile(req, res, next){
-    var s3 = new AWS.S3();
-    var thebucket = {Bucket:"borgellaaws"};
-    
-    s3.createBucket(thebucket, function() {
-        var params = {Bucket: thebucket, Key: keyName, Body: 'Hello World!'};
-        s3.putObject(params, function(err, data) {
-            if (err){
-            console.log(err);
+function formidableReading(req, res, next){
+    var formidable = new Formidable.IncomingForm();
+    formidable.parse(req, function(error, fields, file){
+        if(error){
+            next(new Error('file does not exists'))
+        }else {
+            console.log(fields);
+            console.log(file);
             next();
-            }else
-            console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
-            next();
-        });
+        }
     });
+
 }
 
 module.exports = router;
